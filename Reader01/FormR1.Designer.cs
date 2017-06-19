56f109a4e017c37db3b7d8de972b97e44ba2d840
moroz69off@outlook.com <moroz69off@outlook.com>
2017-06-19 20:30:21 +0300

    emptyFormWithBowser_i_button

---
+++ Reader01/FormR1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace Reader01
+{
+    partial class FormR
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormR));
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.buttonTemp = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(736, 479);
+            this.webBrowser.TabIndex = 0;
+            // 
+            // buttonTemp
+            // 
+            this.buttonTemp.Location = new System.Drawing.Point(836, 56);
+            this.buttonTemp.Name = "buttonTemp";
+            this.buttonTemp.Size = new System.Drawing.Size(75, 23);
+            this.buttonTemp.TabIndex = 1;
+            this.buttonTemp.Text = "button temp";
+            this.buttonTemp.UseVisualStyleBackColor = true;
+            this.buttonTemp.Click += new System.EventHandler(this.buttonTemp_Click);
+            // 
+            // FormR
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(923, 501);
+            this.Controls.Add(this.buttonTemp);
+            this.Controls.Add(this.webBrowser);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(939, 540);
+            this.Name = "FormR";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.Text = "Reader";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.Button buttonTemp;
+    }
+}
+
