31f82ad7283ab7435f4ab601120a46f01544effd
moroz69off@outlook.com <moroz69off@outlook.com>
2017-06-20 00:34:26 +0300

    RedFlags

---
+++ Reader01/VerticalProgressBar.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Forms;
+
+namespace Reader01
+{
+    class VerticalProgressBar : ProgressBar
+    {
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                const int PBS_VERTICAL = 0x04;
+                CreateParams cp = base.CreateParams;
+                cp.Style |= PBS_VERTICAL;
+                return cp;
+            }
+        }
+    }
+}
